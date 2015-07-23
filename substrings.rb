2c6f2fe2b0c6dc2f33e5b4c97e132501a5b0d6ee
Casey Koellner <koellner.casey73@gmail.com>
2015-07-23 18:47:39 -0400

    Tests passed

--- substrings.rb
+++ substrings.rb
@@ -1,3 +1,15 @@
 def substrings(text,dictionary)
-	return {"below"=>1, "low"=>1}
+	#Separates text into an array of individual words
+	words = text.scan(/\w+/)
+	tempArray = []
+	returnHash = Hash.new(0)
+	#Each word is compared to every entry in the dictionary for matches
+	words.each do |word|
+		dictionary.each do |entry|
+			if(word.downcase.match(entry))
+				returnHash[entry] += 1
+			end
+		end
+	end
+	return returnHash
 end
